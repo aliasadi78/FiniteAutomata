14cf4fc7dda3f96e77b878ae0e73ae30e0691f24
Ali Asadi <4.aliasad.4@gmail.com>
2019-05-18 18:47:38 +0430

    1

--- PFA/pfa.py
+++ PFA/pfa.py
@@ -8,18 +8,38 @@ for i in readme:
     b.append(a)
 #     print(b[i])
 # print(b)
-start_state = ['q0']
+start_state = []
 final_state = []
 c =[]
 d = []
+d1 = []
 e = []
+e1 = []
 for i in range(len(b)):
     if i > 1:
         c = b[i].split(",")
         d = c[2].split(" ")
+        d1 = c[2].split(" ")
         e = d[0][0]
+        e1 = d[0][0]
         if e[0] == "*":
             final_state.append(d[0])
+        if e1[0] == "*":
+            final_state.append(d1[0])
 
-fs = list(set(final_state))
-print(fs)
+final_state = list(set(final_state))
+print(final_state)
+for i in range(len(b)):
+    if i > 1:
+        c = b[i].split(",")
+        d = c[0].split(" ")
+        d1 = c[0].split(" ")
+        e = d[0][0]
+        e1 = d[0][0]
+        if e[0] == "-":
+            start_state.append(d[0])
+        if e1[0] == "-":
+            start_state.append(d1[0])
+
+start_state = list(set(start_state))
+print(start_state)
