acde48b7055faf90757a29294ded3b6c10d4201d
Ali Asadi <4.aliasad.4@gmail.com>
2019-05-19 18:39:15 +0430

    1S

--- PFA/pfa.py
+++ PFA/pfa.py
@@ -47,46 +47,68 @@ start_state = list(set(start_state))
 
 a = b[2].replace('->','')
 b[2] = a
-print(b)
+bn = b
 
+b.remove(b[0])
+b.remove(b[0])
+print(b)
 A = []
 B = []
 T = []
 A1 = []
 B1 = []
 T1 = []
-for i in range(2,len(b)):
-    B1 = []
-    A1 = b[i].split(',')
-    # print(A1)
-    B1.append(A1[0])
-    # print(B1)
-    for j in range(2,len(b)):
+
+tb = b
+S = len(tb)
+v = 0
+for i in range(S):
+    if S > 0:
+        B1 = []
+        if i > 0:
+            i -= v
+        A1 = tb[i].split(',')
+        # print(A1)
+        B1.append(A1[0])
+        # print(B1)
         a1 = []
         b1 = []
         t1 = []
-        Q = b[j].split(',')
+        u = 0
+        for j in range(S):
 
-        if B1[0] == Q[0]:
-            # print("11")
-            if Q[1] == "a":
-                a1.append(Q[2])
-                # print(a1)
-                # print("1")
-                # print(Q[2])
-            if Q[1] == "b":
-                b1.append(Q[2])
-                # print(b1)
-                # print("2")
-                # print(Q[2])
-            if Q[1] == "_":
-                t1.append(Q[2])
-                # print(t1)
-                # print("3")
-                # print(Q[2])
-A.append(a1)
-B.append(b1)
-T.append(t1)
+            if j > 0:
+                j -= u
+            Q = tb[j].split(',')
+
+            if B1[0] == Q[0]:
+                del tb[j]
+                u += 1
+                S = len(tb)
+
+                # print("11")
+                if Q[1] == "a":
+                    a1.append(Q[2])
+                    # print(a1)
+                    # print("1")
+                    # print(Q[2])
+                if Q[1] == "b":
+                    b1.append(Q[2])
+                    # print(b1)
+                    # print("2")
+                    # print(Q[2])
+                if Q[1] == "_":
+                    t1.append(Q[2])
+                    # print(t1)
+                    # print("3")
+                    # print(Q[2])
+            else:
+                v += 1
+                break
+
+        A.append(a1)
+        B.append(b1)
+        T.append(t1)
 
 
 
