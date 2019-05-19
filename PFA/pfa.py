c9b9a8e7564da4fb2817490e1e6f34d3ec402fc4
Ali Asadi <4.aliasad.4@gmail.com>
2019-05-19 17:09:10 +0430

    1

--- PFA/pfa.py
+++ PFA/pfa.py
@@ -2,15 +2,16 @@
 
 readme = open('input.txt','r').readlines()
 a = ''
+bb = []
 b = []
 for i in readme:
     a = i.replace('\n','')
     b.append(a)
 #     print(b[i])
-# print(b)
+
 start_state = []
 final_state = []
-c =[]
+c = []
 d = []
 d1 = []
 e = []
@@ -28,7 +29,7 @@ for i in range(len(b)):
             final_state.append(d1[0])
 
 final_state = list(set(final_state))
-print(final_state)
+# print(final_state)
 for i in range(len(b)):
     if i > 1:
         c = b[i].split(",")
@@ -42,4 +43,85 @@ for i in range(len(b)):
             start_state.append(d1[0])
 
 start_state = list(set(start_state))
-print(start_state)
+# print(start_state)
+
+a = b[2].replace('->','')
+b[2] = a
+print(b)
+
+A = []
+B = []
+T = []
+A1 = []
+B1 = []
+T1 = []
+for i in range(2,len(b)):
+    B1 = []
+    A1 = b[i].split(',')
+    # print(A1)
+    B1.append(A1[0])
+    # print(B1)
+    for j in range(2,len(b)):
+        a1 = []
+        b1 = []
+        t1 = []
+        Q = b[j].split(',')
+
+        if B1[0] == Q[0]:
+            # print("11")
+            if Q[1] == "a":
+                a1.append(Q[2])
+                # print(a1)
+                # print("1")
+                # print(Q[2])
+            if Q[1] == "b":
+                b1.append(Q[2])
+                # print(b1)
+                # print("2")
+                # print(Q[2])
+            if Q[1] == "_":
+                t1.append(Q[2])
+                # print(t1)
+                # print("3")
+                # print(Q[2])
+A.append(a1)
+B.append(b1)
+T.append(t1)
+
+
+
+
+
+print(A)
+print(B)
+print(T)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
