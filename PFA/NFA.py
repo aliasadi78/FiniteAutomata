23bbaab87dc52fc2d53cf176d8610a523083b62e
Ali Asadi <4.aliasad.4@gmail.com>
2019-05-22 14:04:47 +0430

    a

--- PFA/NFA.py
+++ PFA/NFA.py
@@ -5,7 +5,13 @@ class NODE:
             self.Nueighbor[key] = []
         self.Final_state=bool()
         self.Name = name
-
+class Node_Dfa:
+    def __init__(self,name,Alphabet):
+        self.Nueighbor = dict()
+        for key in Alphabet:
+            self.Nueighbor[key] = []
+        self.Final_state=bool()
+        self.Name = name
 class NFA:
     def __init__(self,Alphabet,Number_state):
         self.Start_Variable = None
@@ -16,10 +22,21 @@ class NFA:
             name = 'q' + str(number)
             new_node = NODE(name,self.Alphabet)
             self.States += [new_node]
+class DFA:
+    def __init__(self, Alphabet):
+        self.Start_Variable = None
+        self.Alphabet = Alphabet
+        self.States = []
+        for number in range(len(self.States)):
+            name = 'q' + str(number)
+            new_node = NODE(name, self.Alphabet)
+            self.States += [new_node]
 
 Lines = open("input.txt",'r').readlines()
 Alphabet = Lines[1].replace('\n','').split(',')
+Alphabet2 = Alphabet.copy()
 Alphabet.append("_")
+
 NFA = NFA(Alphabet,int(Lines[0]))
 for line in range(2,len(Lines)):
     info = Lines[line].split(',')
@@ -27,8 +44,5 @@ for line in range(2,len(Lines)):
     destination_index = int(info[2].replace('\n','').split('q')[1])
     NFA.States[origin_index].Nueighbor[info[1]] += [NFA.States[destination_index]]
 
-# print(NFA.States)
-print(NFA.States[0].Nueighbor)
-# for i in range(NFA.Number_State):
-#     NFA.Number_State[i]
-def Convert():
+
+
